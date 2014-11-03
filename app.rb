bebcae519276cae36475002d498de13494ef350f
Jason Benn <jasoncbenn@gmail.com>
2014-11-02 23:03:06 -0800

    Add Tinder model

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 Bundler.require :web
 Bundler.require :development if development?
+require_relative 'tinder'
+
+also_reload 'tinder.rb'
 
 get '/style.css' do
   less :style
 end
 
 get '/' do
+  # Tinder.sign_in
+  Tinder.refresh_subscribed_users
+  # p Tinder.fetch_updates
+  @matches = Tinder.sort_by_matched
+  @online = Tinder.sort_by_recently_online
   haml :index
 end
 
